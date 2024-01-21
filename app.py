ec0e69227e28a2b394b5416d71032faa7c4f95e6
abhiraj <abhirajsachan706@hmail.com>
2024-01-21 14:14:09 +0530

    resume

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
+resume_file = current_dir / "assets" / "A.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
 
 
@@ -40,11 +40,11 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 
 # --- LOAD CSS, PDF & PROFIL PIC ---
-with open(css_file) as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-with open(resume_file, "rb") as pdf_file:
-    PDFbyte = pdf_file.read()
-profile_pic = Image.open(profile_pic)
+# with open(css_file) as f:
+#     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+# with open(resume_file, "rb") as pdf_file:
+#     PDFbyte = pdf_file.read()
+# profile_pic = Image.open(profile_pic)
 
 
 # --- HERO SECTION ---
